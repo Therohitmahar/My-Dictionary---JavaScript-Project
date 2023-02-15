78fca19e5d57ca2d3d22d0f4f1e44b07c47a2ff0
rohit mahar <therohitmahar@gmail.com>
2023-02-15 18:15:45 +0530

    first commit

--- App.js
+++ App.js
@@ -1,1 +1,42 @@
 
+const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const result = document.getElementById('result');
+const sound = document.getElementById('sound');
+const btn =document.getElementById('search-btn');
+
+function searchWord(){
+    let inputWord = document.getElementById('input').value;
+
+    fetch(`${url}${inputWord}`)
+    .then((response) => response.json())
+    .then((data)=> {
+        console.log(data);
+        result.innerHTML = `<div class="word">
+        <h3>${inputWord}</h3>
+        <button onClick="playSound()"><img src="img/volume-up-interface-symbol.png"</button>
+    </div>
+    <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech} </p>
+        <p>/${data[0].phonetic} /</p>
+    </div>
+    <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+    </p>
+    <p class="word-example">
+       ${data[0].meanings[0].definitions[0].example || ""}
+    </p>`;
+    sound.setAttribute("src",`${data[0].phonetics[0].audio}`);
+    
+        
+    
+})
+.catch(()=>{
+        result.innerHTML= `<h3 class="error" >Coudn't Find The Word '${inputWord}'</h3>`
+    })
+}
+btn.addEventListener('click', searchWord);
+
+function playSound(){
+    sound.play();
+}
